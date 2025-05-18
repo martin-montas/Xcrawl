2c765cb2b08cb65924e04b7032c659888019af0b
Martin Montas <martinmontas1@gmail.com>
2025-05-17 22:00:29 -0400

    Can now get the hrefs tags of websites

--- crawl.go
+++ crawl.go
@@ -1,82 +1,108 @@
 package main
 
-import ( 
+import (
+	"fmt"
+	"io"
 	"log"
-	"golang.org/x/net/html"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
+
+	//"github.com/go-playground/locales/root"
+	"golang.org/x/net/html"
 )
 
-var links []string
+var links 			[]string
+var elements 	= 	[]string {"a", "base", "area", "link"}
+var Nodes  [] html.Node
 
-func extractLinks(currDomain string) ([]string, error) {
+
+func sendHTTPRequestAndParse(currDomain string) *html.Node {
 	// Slice to store extracted href links
-
 	resp, err := http.Get(currDomain)
+	fmt.Println(time.Now().Format("2006-01-02 03:04:05 PM"),
+		"[\033[32mINFO\033[0m]", currDomain)
+
 	if err != nil {
 		log.Fatal(err)
-		return links, err
 	}
-	defer resp.Body.Close()
 
+	defer resp.Body.Close()
 	// TODO:(parse the body here for href hyperlinks of any type of reference to a
 	// a page on the same domain and append it to the urls slice)
 	// Print the response body
 
 	// Parse the HTML from the response
-	doc, err := html.Parse(resp.Body)
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Recursive function to traverse the HTML tree
-	var extractHrefs func(*html.Node)
-	extractHrefs = func(n *html.Node) {
-
-		// If the node is an <a> tag, get the href attribute
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, attr := range n.Attr {
-				if attr.Key == "href" {
-					// process the domain
-					ok, fullURL, err := validateDomain(currDomain, attr.Val)
-					
-					if err == nil {
-						log.Fatal(err)
-					}
-
-					if ok {
-						links = append(links, fullURL)
-					}
-				}
-			}
-		}
-
-		// Recursively traverse child nodes
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			extractHrefs(c)
-		}
+	bodyString := string(bodyBytes)
+	// fmt.Println(bodyString)
+	doc, err := html.Parse(strings.NewReader(bodyString))
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	// Start extracting href attributes from the root of the HTML tree
-	extractHrefs(doc)
-	return links, nil
+	return doc
 }
 
+func run(rootDomain string) {
+	doc := sendHTTPRequestAndParse(rootDomain)
+	extractLinks(*doc)
+}
+
+func extractLinks(doc html.Node)   {
+    if doc.Type == html.ElementNode && doc.Data == "a" {
+        for _, attr := range doc.Attr {
+            if attr.Key == "href" {
+                fmt.Println("Link:", attr.Val)
+            }
+        }
+        if doc.FirstChild != nil && doc.FirstChild.Type == html.TextNode {
+            fmt.Println("Text:", doc.FirstChild.Data)
+        }
+    }
+    for c := doc.FirstChild; c != nil; c = c.NextSibling {
+        extractLinks(*c)
+    }
+}
+	// }
+// }
 func validateDomain(baseSite string, foundURI string) (bool, string, error) {
 	baseURL, err := url.Parse(baseSite)
 	if err != nil {
+		fmt.Println("fuck!!")
 		return false, "", err
 	}
-
 	targetURL, err := url.Parse(foundURI)
 	if err != nil {
 		return false, "", err
 	}
-
 	// Resolve relative URI to absolute
 	resolvedURL := baseURL.ResolveReference(targetURL)
 
 	// Compare hostnames
+	fmt.Println(resolvedURL.String())
 	return resolvedURL.Host == baseURL.Host, resolvedURL.String(), nil
 }
 
+func findDupplicateLinksArray(links []string) []string {
+	for ind, link := range links {
+		for ind_2, link_2 := range links {
+			if ind == ind_2 {
+				continue
+			} else if link == link_2 {
+				removeAtIndex(links, ind_2)
+			}
+		}
+	}
+	return links
+}
+
+func removeAtIndex(s []string, i int) []string {
+	return append(s[:i], s[i+1:]...)
+}
+
+func catToFile() { }
+func pingDomain() { }
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import ( 
-		"fmt"
-		"log"
-		"time"
-		"flag"
+	"fmt"
+	"time"
+	"flag"
 )
 
 var urls []string
@@ -12,9 +11,9 @@ var rootDomain string
 
 func main() {
 	url 				:= flag.String("u", "", "Name to greet")
-	verbose 			:= flag.Bool("v", true, "Enable verbose output.")
+	verbose 			:= flag.Bool("v", false, "Enable verbose output.")
 	thread 				:= flag.Int("t", 3, "The amount of threads.")
-	files 				:= flag.String("o", "", "File to save the domains and params found.")
+	files 				:= flag.String("o", "", "Output file.")
 	flag.Parse()
 	fmt.Print("\033[34m")
 	fmt.Println(`
@@ -25,24 +24,29 @@ func main() {
 		██║╚██╗██║██║   ██║██║     ██╔═██╗ 
 		██║ ╚████║╚██████╔╝╚██████╗██║  ██╗
 		╚═╝  ╚═══╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
-		
+
 		Nock Url/Param Crawler.
 		v0.0.1
 
 		coder: @github.com/martin-montas
 
-	`)
+		`)
 	fmt.Print("\033[0m")
 	if *verbose {
 		fmt.Println(
-			time.Now().Format("2006-01-02 03:04:05 PM"), 
+			time.Now().Format("2006-01-02 03:04:05 PM"),
 			"[\033[32mINFO\033[0m] Verbose mode on")
+	} 
+	if !*verbose {
+		fmt.Println(
+			time.Now().Format("2006-01-02 03:04:05 PM"),
+			"[\033[32mINFO\033[0m] Verbose mode off")
 	}
 
 	fmt.Println(time.Now().Format("2006-01-02 03:04:05 PM"),
-		"[\033[32mINFO\033[0m] using" ,
+		"[\033[32mINFO\033[0m] using",
 		*thread,
-	"threads")
+		"threads")
 
 	if *files != "" {
 		fmt.Println(time.Now().Format("2006-01-02 03:04:05 PM"),
@@ -52,13 +56,8 @@ func main() {
 	if *url != "" {
 		fmt.Println(time.Now().Format("2006-01-02 03:04:05 PM"),
 			"[\033[32mINFO\033[0m]", *url, "Will be used as root domain.")
-
 		rootDomain = *url
-		links, err := extractLinks(rootDomain)
-
-		if err != nil {
-			log.Fatal(err)
-		}
+		run(rootDomain)
 
 		for _, val := range links {
 			fmt.Println(val)
@@ -67,5 +66,5 @@ func main() {
 
 	if *url == "" {
 		fmt.Println("An URL should be given.")
-		}
+	}
 }
